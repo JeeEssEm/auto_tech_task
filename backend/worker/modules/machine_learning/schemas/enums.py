9b3680e9b58faaff90cbb01fa1256f52709fce87
JeeEssEm <greendiesel228@gmail.com>
2026-03-22 03:12:18 +0300

    выпилена устаревшая реализация

--- backend/worker/modules/machine_learning/schemas/enums.py
+++
@@ -1,9 +0,0 @@
-﻿import enum
-
-
-class TemplateType(enum.StrEnum):
-    GOST19 = "gost19"
-    FREE = "free"
-    IT_PROJECT = "it_project"
-    CONSTRUCTION_PROJECT = "construction_project"
-    ENGINEERING_PROJECT = "engineering_project"
